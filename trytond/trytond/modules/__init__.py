3fa90b86bd1c085fe4d7138d95b4e2da67fb216c
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:04:27 +0200

    [trytond] Actually clear caches when updating [CUSTOM]
    Warning: this was tested with the MemoryCache and a
    postgresql-synced cache clearing (clean_timeout=0)
    
    Rather than clearing nothing, we delay the cache clear
    until after all modules were updated.
    
    PR: #130
    Fix #18995

--- trytond/trytond/modules/__init__.py
+++ trytond/trytond/modules/__init__.py
@@ -217,6 +217,8 @@ def load_module_graph(graph, pool, update=None, lang=None):
 
         idx = 0
         count = len(modules)
+        # JCA : Delay cache clears
+        caches_to_clear = []
         for node in graph:
             module = node.name
             if module not in MODULES:
@@ -302,6 +304,8 @@ def load_module_graph(graph, pool, update=None, lang=None):
             Model.clean()
             ModelField = pool.get('ir.model.field')
             ModelField.clean()
+            Cache._reset[transaction] = set(
+                caches_to_clear + list(Cache._reset.get(transaction, [])))
             transaction.commit()
 
         # JCA: Add update parameter to post init hooks
