338f5728901b812a6d3d8217583b5294549a382a
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:35 +0200

    [trytond] trytond: Sorting actions by their decoded names [CUSTOM] (#235)
    Fix #PJAZZ-704

--- trytond/trytond/ir/action.py
+++ trytond/trytond/ir/action.py
@@ -3,7 +3,7 @@
 import os
 from collections import defaultdict
 from functools import partial
-from operator import itemgetter
+from unidecode import unidecode
 
 from genshi.template.text import TextTemplate
 
@@ -312,7 +312,7 @@ class ActionKeyword(ModelSQL, ModelView):
                         if parent:
                             value['name'] = (
                                 parent.rec_name + ' / ' + value['name'])
-        keywords.sort(key=itemgetter('name'))
+        keywords.sort(key=lambda x: unidecode(x['name']))
         cls._get_keyword_cache.set(key, keywords)
         return keywords
 
