317be8422a95ca69ddc79e254bcaaa54d8ec6b2d
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:04:04 +0200

    [modules/party] Correction of test on the party module [CUSTOM]

--- modules/party/category.py
+++ modules/party/category.py
@@ -6,8 +6,11 @@ from sql.operators import Equal
 from trytond.model import (
     DeactivableMixin, Exclude, ModelSQL, ModelView, fields, tree)
 
+SEPARATOR = ' / '
 
-class Category(DeactivableMixin, tree(separator=' / '), ModelSQL, ModelView):
+
+class Category(
+        DeactivableMixin, tree(separator=SEPARATOR), ModelSQL, ModelView):
     "Category"
     __name__ = 'party.category'
     name = fields.Char(
@@ -30,3 +33,33 @@ class Category(DeactivableMixin, tree(separator=' / '), ModelSQL, ModelView):
                 'party.msg_category_name_unique'),
             ]
         cls._order.insert(0, ('name', 'ASC'))
+
+    @classmethod
+    def validate(cls, categories):
+        super(Category, cls).validate(categories)
+        cls.check_recursion(categories)
+        for category in categories:
+            category.check_name()
+
+    def check_name(self):
+        if SEPARATOR in self.name:
+            self.raise_user_error('wrong_name', (self.name,))
+
+    def get_rec_name(self, name):
+        if self.parent:
+            return self.parent.get_rec_name(name) + SEPARATOR + self.name
+        return self.name
+
+    @classmethod
+    def search_rec_name(cls, name, clause):
+        if isinstance(clause[2], str):
+            values = clause[2].split(SEPARATOR)
+            values.reverse()
+            domain = []
+            field = 'name'
+            for name in values:
+                domain.append((field, clause[1], name))
+                field = 'parent.' + field
+            return domain
+        # TODO Handle list
+        return [('name',) + tuple(clause[1:])]
