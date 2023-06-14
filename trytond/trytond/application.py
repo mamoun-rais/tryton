f7e70f80976464d18d213a5e397c9f45b93dcbc1
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:04:24 +0200

    [trytond] Make wsgi application pypy compatible [CUSTOM]
    The pypy module of wsgi does not allow to specify the name
    of the variable to use as the application, so we must
    expose an 'application' attribute
    
    PR: #103
    Fix #00000

--- trytond/trytond/application.py
+++ trytond/trytond/application.py
@@ -7,7 +7,7 @@ import os
 import threading
 from io import StringIO
 
-__all__ = ['app']
+__all__ = ['app', 'application']
 
 LF = '%(process)s %(thread)s [%(asctime)s] %(levelname)s %(name)s %(message)s'
 log_file = os.environ.get('WSGI_LOG_FILE')
@@ -46,4 +46,5 @@ if db_names:
     for thread in threads:
         thread.join()
 
+application = app
 assert len(threads := threading.enumerate()) == 1, f"len({threads}) != 1"
