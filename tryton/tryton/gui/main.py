8edd245ab66fa66ff7ee14caf825d5362391f112
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:05:19 +0200

    [tryton] Avoid concurrent calls to global_search [CUSTOM]
    PR: (#18)
    Fix #6786

--- tryton/tryton/gui/main.py
+++ tryton/tryton/gui/main.py
@@ -247,6 +247,9 @@ class Main(Gtk.Application):
         self.current_page = 0
         self.last_page = 0
         self.dialogs = []
+        self._global_run = False
+        self._global_check_timeout_id = None
+        self._global_update_timeout_id = None
 
         # Register plugins
         tryton.plugins.register()
@@ -359,6 +362,8 @@ class Main(Gtk.Application):
 
         def update(widget, search_text, callback=None):
             def end():
+                self._global_update_timeout_id = None
+                self._global_run = False
                 if callback:
                     callback()
                 return False
@@ -378,6 +383,8 @@ class Main(Gtk.Application):
                 except RPCException:
                     result = []
                 if search_text != widget.get_text():
+                    self._global_update_timeout_id = None
+                    self._global_run = False
                     if callback:
                         callback()
                     return False
@@ -399,6 +406,7 @@ class Main(Gtk.Application):
                 widget.emit('changed')
                 end()
 
+            self._global_run = True
             RPCExecute('model', 'ir.model', 'global_search', search_text,
                 CONFIG['client.limit'], self.menu_screen.model_name,
                 context=self.menu_screen.context, callback=set_result,
@@ -426,7 +434,7 @@ class Main(Gtk.Application):
 
         def changed(widget):
             search_text = widget.get_text()
-            GLib.timeout_add(300, update, widget, search_text)
+            check_timeout(widget, search_text)
 
         def activate(widget):
             def message():
