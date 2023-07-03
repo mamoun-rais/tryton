23f7bab09d7d16068bf4c2b0b577356fe8af1ccd
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-07-03 16:18:18 +0200

    [modules/product] Mono-repo migration for coog-2.10

--- modules/product/product.py
+++ modules/product/product.py
@@ -207,15 +207,6 @@ class Template(
         Product.sync_code(products)
 
     @classmethod
-    def copy(cls, templates, default=None):
-        if default is None:
-            default = {}
-        else:
-            default = default.copy()
-        default.setdefault('code', None)
-        return super().copy(templates, default=default)
-
-    @classmethod
     def search_global(cls, text):
         for record, rec_name, icon in super(Template, cls).search_global(text):
             icon = icon or 'tryton-product'
@@ -254,14 +245,6 @@ class TemplateFunction(fields.Function):
             return getattr(Template, name).convert_order(
                 name, tables['template'], Template)
         return order
-
-    def definition(self, model, language):
-        pool = Pool()
-        Template = pool.get('product.template')
-        definition = super().definition(model, language)
-        definition['searchable'] = self._field.definition(
-            Template, language)['searchable']
-        return definition
 
 
 class Product(
