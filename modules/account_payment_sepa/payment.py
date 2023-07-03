15a23ad5139998ac5b21541a7ba57fa969fc4a14
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-07-03 16:23:03 +0200

    [modules/account_payment_sepa] Mono-repo migration for coog-2.8

--- modules/account_payment_sepa/payment.py
+++ modules/account_payment_sepa/payment.py
@@ -441,11 +441,16 @@ class Mandate(Workflow, ModelSQL, ModelView):
                     'depends': ['state'],
                     },
                 })
-        t = cls.__table__()
-        cls._sql_constraints = [
-            ('identification_unique', Unique(t, t.company, t.identification),
-                'account_payment_sepa.msg_mandate_unique_id'),
-            ]
+        # t = cls.__table__()
+        # JMO/RSE 2017_04_18 Following 4929e02594d
+        # We override in coog the possibility to keep the mandate
+        # for several bank account but we suffer from the register order
+        # if we try to delete the constraint from coog module
+        # t = cls.__table__()
+        # cls._sql_constraints = [
+        #     ('identification_unique', Unique(t, t.company, t.identification),
+        #         'account_payment_sepa.msg_mandate_unique_id'),
+        #     ]
 
     @staticmethod
     def default_company():
