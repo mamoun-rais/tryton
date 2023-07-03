287adc0d855139201a71034822a8229ece07522c
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-07-03 16:21:21 +0200

    [modules/account_payment] Mono-repo migration for coog-22.14

--- modules/account_payment/payment.py
+++ modules/account_payment/payment.py
@@ -328,6 +328,16 @@ class Payment(Workflow, ModelSQL, ModelView):
             ('failed', 'Failed'),
             ], 'State', readonly=True, select=True)
 
+    @property
+    def amount_line_paid(self):
+        if self.state != 'failed':
+            if self.line.second_currency:
+                payment_amount = abs(self.line.amount_second_currency)
+            else:
+                payment_amount = abs(self.line.credit - self.line.debit)
+            return max(min(self.amount, payment_amount), 0)
+        return Decimal(0)
+
     @classmethod
     def __setup__(cls):
         super(Payment, cls).__setup__()
@@ -579,3 +589,6 @@ class ProcessPayment(Wizard):
         return action, {
             'res_id': [g.id for g in groups],
             }
+
+    def default_start(self, name):
+        return {}
