7ef6dfbcf4cd2f65fcf13a0183d8412da2868358
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-09-29 20:35:01 +0200

    sao: Fix missing var initialization [CUSTOM]

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -1410,8 +1410,9 @@
                 var row = rows[i];
                 var record = row.record;
                 var field_name;
+                var column;
                 for (var j=0; j < row.tree.columns.length; j++) {
-                    var column = row.tree.columns[j];
+                    column = row.tree.columns[j];
                     if (column.type == 'field') {
                         field_name = column.attributes.name;
                         break;
