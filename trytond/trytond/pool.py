2bd9ad24e123a6a03d06ae6c2aa6a8d5c3c16f65
Jean Cavallo <jean.cavallo@hotmail.fr>
2024-03-13 15:52:53 +0100

    Add final migration hooks [CUSTOM] (#163)
    Fix PCLAS-686

--- trytond/trytond/pool.py
+++ trytond/trytond/pool.py
@@ -62,6 +62,8 @@ class Pool(object):
     test = False
     _init_hooks = {}
     _post_init_calls = {}
+    _registered_migration_hooks = {}
+    _final_migrations = {}
     _registered_notifications = {}
     _notification_callbacks = {}
     pool_types = {'model', 'report', 'wizard'}
@@ -131,6 +133,13 @@ class Pool(object):
         Pool._init_hooks[kwargs['module']] += hooks
 
     @staticmethod
+    def register_final_migration(*hooks, module=None):
+        assert module is not None
+        if module not in Pool._registered_migration_hooks:
+            Pool._registered_migration_hooks[module] = []
+        Pool._registered_migration_hooks[module] += hooks
+
+    @staticmethod
     def register_notification_callbacks(keyword, callback, *, module=None):
         if module not in Pool._registered_notifications:
             Pool._registered_notifications[module] = {}
@@ -184,6 +193,7 @@ class Pool(object):
             for type in self.classes.keys():
                 self._pool[self.database_name][type] = {}
             self._post_init_calls[self.database_name] = []
+            self._final_migrations[self.database_name] = []
             self._notification_callbacks[self.database_name] = {}
             try:
                 with ServerContext().set_context(disable_auto_cache=True):
@@ -203,6 +213,10 @@ class Pool(object):
     def post_init(self, update):
         for hook in self._post_init_calls[self.database_name]:
             hook(self, update)
+
+    def final_migrations(self):
+        for migration in self._final_migrations[self.database_name]:
+            migration(self)
 
     def get(self, name, type='model'):
         '''
@@ -273,6 +287,8 @@ class Pool(object):
                 classes[type_].append(cls)
         self._post_init_calls[self.database_name] += self._init_hooks.get(
             module, [])
+        self._final_migrations[self.database_name] += \
+            self._registered_migration_hooks.get(module, [])
         self._notification_callbacks[self.database_name].update(
             self._registered_notifications.get(module, {}))
         self._modules.append(module)
