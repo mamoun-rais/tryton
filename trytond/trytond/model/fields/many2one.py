fb7a89a1b5045db649a225cf8afa992a4f2892ae
Jean Cavallo <jean.cavallo@hotmail.fr>
2024-05-30 17:48:35 +0200

    Correctly override _convert_domain [CUSTOM] (#221)
    Fix PCLAS-901

--- trytond/trytond/model/fields/many2one.py
+++ trytond/trytond/model/fields/many2one.py
@@ -197,8 +197,7 @@ class Many2One(Field):
         return expression
 
     @inactive_records
-    @domain_method
-    def convert_domain(self, domain, tables, Model):
+    def _convert_domain(self, domain, tables, Model):
         pool = Pool()
         Rule = pool.get('ir.rule')
         Target = self.get_target()
@@ -257,7 +256,7 @@ class Many2One(Field):
                 return expression
 
             if not isinstance(value, str):
-                return super(Many2One, self).convert_domain(domain, tables,
+                return super(Many2One, self)._convert_domain(domain, tables,
                     Model)
             else:
                 target_name = 'rec_name'
