fe12c1b560d4195107ed99f8ad0e1d39ed8b4e90
Jean Cavallo <jean.cavallo@hotmail.fr>
2024-09-19 09:02:06 +0200

    Revert "account_statement: Add amount_second_currency in line group table query [PREVIEW]"
    This reverts commit bddcbbe3d0dfb9d44f4f603a8485c7d7293a1563.

--- modules/account_statement/statement.py
+++ modules/account_statement/statement.py
@@ -1150,7 +1150,6 @@ class LineGroup(ModelSQL, ModelView):
             move.write_uid,
             move.write_date,
             line.second_currency,
-            line.amount_second_currency,
             ]
 
         columns = (std_columns + [move.id.as_('move')]
